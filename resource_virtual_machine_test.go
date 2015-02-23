fa5755d277d4df1ba7e15315a9dc91a8e0ebc8e5
Michael Kuzmin <mkuzmin@gmail.com>
2015-02-23 21:09:23 +0300

    checkDestroy - incomplete

--- resource_virtual_machine_test.go
+++ resource_virtual_machine_test.go
@@ -5,6 +5,8 @@ import (
     "fmt"
     "github.com/hashicorp/terraform/helper/resource"
     "github.com/hashicorp/terraform/terraform"
+    "github.com/vmware/govmomi"
+    "github.com/vmware/govmomi/vim25/types"
 )
 
 // Requirements:
@@ -12,6 +14,7 @@ import (
 // folder: <root>
 // VM tools installed
 func TestAccVirtualMachine_basic(t *testing.T) {
+    var vm_id string
     resource.Test(t, resource.TestCase{
         PreCheck:  func() { testAccPreCheck(t) },
         Providers: testAccProviders,
@@ -19,39 +22,49 @@ func TestAccVirtualMachine_basic(t *testing.T) {
             resource.TestStep{
                 Config: testAccVirtualMachine_basic,
                 Check: resource.ComposeTestCheckFunc(
-                testAccCheckVirtualMachineState("name", "basic-1"),
+                testAccCheckVirtualMachineState(&vm_id),
                 ),
             },
         },
-        CheckDestroy: testAccCheckVirtualMachineDestroy,
-    })
+        CheckDestroy: resource.ComposeTestCheckFunc(
+            testAccCheckVirtualMachineDestroy(&vm_id),
+        ),
+    },
+    )
 }
 
-func testAccCheckVirtualMachineState(key, value string) resource.TestCheckFunc {
+func testAccCheckVirtualMachineState(vm_id *string) resource.TestCheckFunc {
     return func(s *terraform.State) error {
         rs, ok := s.RootModule().Resources["vsphere_virtual_machine.app"]
         if !ok {
             return fmt.Errorf("Not found: %s", "vsphere_virtual_machine.app")
         }
 
-        if rs.Primary.ID == "" {
-            return fmt.Errorf("No ID is set")
-        }
-
         p := rs.Primary
-        if p.Attributes[key] != value {
-            return fmt.Errorf("%s != %s (actual: %s)", key, value, p.Attributes[key])
+        if p.ID == "" {
+            return fmt.Errorf("No ID is set")
         }
         if p.Attributes["ip_address"] == "" {
             return fmt.Errorf("IP address is not set")
         }
+        *vm_id = p.ID
 
         return nil
     }
 }
 
-func testAccCheckVirtualMachineDestroy(s *terraform.State) error {
-    return fmt.Errorf("####### %s", s)
+func testAccCheckVirtualMachineDestroy(vm_id *string) resource.TestCheckFunc {
+    return func(s *terraform.State) error {
+        client := testAccProvider.Meta().(*govmomi.Client)
+
+        vm_mor := types.ManagedObjectReference{Type: "VirtualMachine", Value: *vm_id }
+        err := client.Properties(vm_mor, []string{"summary"}, &vm_mor)
+        if err == nil {
+            return fmt.Errorf("Record still exists")
+        }
+
+        return nil
+    }
 }
 
 const testAccVirtualMachine_basic = `
